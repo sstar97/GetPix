c164195a0f028e8582281b0479e5b29ca37267a8
sstar97 <semihyildiz1997@gmail.com>
2021-12-25 07:52:31 +0300

    GetPix v0.1
    GetPix v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace GetPixRGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pitcureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(474, 287);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(474, 313);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(474, 339);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(474, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "İçe Aktar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(474, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Dışa Aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(4, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Siyah-Beyaz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(4, 55);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 30);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Negatif";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Location = new System.Drawing.Point(474, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(83, 199);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtreler";
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(4, 163);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(73, 30);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Mavi";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(4, 127);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(73, 30);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Yeşil";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(4, 91);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 30);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Kırmızı";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(456, 340);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 365);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "GetPixRGB";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetPixRGB
+{
+    public partial class Form1 : Form
+    {
+        bool ngtf, gray, krmz, ysl, mav = true;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Resim Dosyaları|" + "*.bmp;*.jpg;*.gif;*.wmf;*.tif;*.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                pictureBox2.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        //Mouse ile tıklandığında pixeldeki renk kodlarını alma.
+        private void pitcureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen bir fotoğraf yükleyin!");
+            }
+            else
+            {
+                Bitmap b = new Bitmap(pictureBox1.Image);
+                Color color = b.GetPixel(e.X, e.Y);
+                textBox1.Text = "R:" + color.R.ToString();
+                textBox2.Text = "G:" + color.G.ToString();
+                textBox3.Text = "B:" + color.B.ToString();
+            }
+        }
+
+        //Yeşil yapma.
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen bir fotoğraf yükleyin!");
+            }
+            else
+            {
+                ysl = !ysl;
+                
+                Bitmap ys = new Bitmap(pictureBox1.Image);
+
+                int width = ys.Width;
+                int height = ys.Height;
+
+                if (ysl == false)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+
+
+                            Color p = ys.GetPixel(x, y);
+
+                            int a = p.A;
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+
+
+
+                            ys.SetPixel(x, y, Color.FromArgb(a, 0, g, 0));
+                        }
+                    }
+                    pictureBox1.Image = ys;
+                    button6.Text = "Orjinale Döndür";
+                }
+                else
+                {
+                    pictureBox1.Image = pictureBox2.Image;
+                    button6.Text = "Yeşil";
+                }
+            }
+        }
+
+        //Mavi yapma
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen bir fotoğraf yükleyin!");
+            }
+            else
+            {
+                mav = !mav;
+
+                Bitmap mv = new Bitmap(pictureBox1.Image);
+
+                int width = mv.Width;
+                int height = mv.Height;
+
+                if (mav == false)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+
+
+                            Color p = mv.GetPixel(x, y);
+
+                            int a = p.A;
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+
+
+
+                            mv.SetPixel(x, y, Color.FromArgb(a, 0, 0, b));
+                        }
+                    }
+                    pictureBox1.Image = mv;
+                    button7.Text = "Orjinale Döndür";
+                }
+                else
+                {
+
+                    pictureBox1.Image = pictureBox2.Image;
+                    button7.Text = "Mavi";
+                }
+            }
+        }
+
+        //Üzerinde çalışılan fotoğrafı kaydetme.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Title = "Kaydet";
+            saveDialog.Filter = "Resim Dosyaları (*.png;*.jpg)|*.png;*.jpg";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = saveDialog.FileName;
+            }
+        }
+
+        //Negatif yapma.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen bir fotoğraf yükleyin!");
+            }
+            else
+            {
+                ngtf = !ngtf;
+                
+                Bitmap ng = new Bitmap(pictureBox1.Image);
+
+                int width = ng.Width;
+                int height = ng.Height;
+
+                if (ngtf == false)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+
+                        
+                            Color p = ng.GetPixel(x, y);
+
+                            int a = p.A;
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+
+                            r = 255 - r;
+                            g = 255 - g;
+                            b = 255 - b;
+
+                            ng.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                        }
+                    }
+                    pictureBox1.Image = ng;
+                    button4.Text = "Orjinale Döndür";
+                }
+                else
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+
+
+                            Color p = ng.GetPixel(x, y);
+
+                            int a = p.A;
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+
+                            r = 255 - r;
+                            g = 255 - g;
+                            b = 255 - b;
+
+                            ng.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                        }
+                    }
+                    pictureBox1.Image = ng;
+                    button4.Text = "Negatife Çevir";
+                }
+            }
+        }
+
+        //Kırmızı yapma.
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen bir fotoğraf yükleyin!");
+            }
+            else
+            {
+                krmz = !krmz;
+                
+                Bitmap kr = new Bitmap(pictureBox1.Image);
+
+                int width = kr.Width;
+                int height = kr.Height;
+
+                if (krmz == false)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+
+
+                            Color p = kr.GetPixel(x, y);
+
+                            int a = p.A;
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+
+                            
+
+                            kr.SetPixel(x, y, Color.FromArgb(a, r, 0, 0));
+                        }
+                    }
+                    pictureBox1.Image = kr;
+                    button5.Text = "Orjinale Döndür";
+                }
+                else
+                {
+
+                    pictureBox1.Image = pictureBox2.Image;
+                    button5.Text = "Kırmızı";
+                }
+            }
+        }
+
+        //Siyah-Beyaz yapma.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Lütfen bir fotoğraf yükleyin!");
+            }
+            else
+            {
+                gray = !gray;
+                
+                Bitmap gr = new Bitmap(pictureBox1.Image);
+
+                int width = gr.Width;
+                int height = gr.Height;
+
+                if(gray== false)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            Color p = gr.GetPixel(x, y);
+
+                            p = gr.GetPixel(x, y);
+
+                            int a = p.A;
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+
+                            int avrj = (r + g + b) / 3;
+
+                            gr.SetPixel(x, y, Color.FromArgb(a, avrj, avrj, avrj));
+                        }
+                    }
+                    pictureBox1.Image = gr;
+                    button3.Text = "Orjinale Çevir";
+                }
+                else
+                {
+                    pictureBox1.Image = pictureBox2.Image;
+                    button3.Text = "Siyah-Beyaz Çevir";
+                }
+            }
+        }
+
+
+    }
+}
